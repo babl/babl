b5cce1df5003be20cb3d3a0bc499839980d35524
Lars Kluge <l@larskluge.com>
2015-11-30 16:53:09 +0700

    Refactor cli code a bit

--- cli.go
+++ cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/codegangsta/cli"
 	pb "github.com/larskluge/babl/protobuf"
@@ -46,6 +47,65 @@ func pingSubCommands() (cmds []cli.Command) {
 				}
 			},
 		})
+	}
+	return
+}
+
+func configureCli() (app *cli.App) {
+	app = cli.NewApp()
+	app.Usage = "Client to access the Babl Network."
+	app.Version = "0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Usage: "Host to connect to, e.g. babl.sh, localhost",
+			Value: "babl.sh",
+		},
+		cli.IntFlag{
+			Name:   "port",
+			Usage:  "Port to connect to",
+			EnvVar: "PORT",
+			Value:  4444,
+		},
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Verbose logging",
+		},
+	}
+	app.HideHelp = true
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "list-modules",
+			Aliases: []string{"ls"},
+			Usage:   "List all available modules",
+			Action: func(_ *cli.Context) {
+				shared.PrintAvailableModules()
+			},
+		},
+		{
+			Name:        "ping",
+			Usage:       "ping <module>",
+			Subcommands: pingSubCommands(),
+			Action: func(_ *cli.Context) {
+				fmt.Println("Unknown module")
+				os.Exit(3)
+			},
+		},
+		{
+			Name:  "config",
+			Usage: "Print configuration",
+			Action: func(_ *cli.Context) {
+				fmt.Println(Config())
+			},
+		},
+	}
+
+	for _, module := range shared.Modules() {
+		appendModuleCommand(&app.Commands, module)
+	}
+	for module, _ := range Config().Defaults {
+		appendModuleCommand(&app.Commands, module)
 	}
 	return
 }
