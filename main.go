c0b36e20e7e00074be58a682c48e9b20ef6328fb
Lars Kluge <l@larskluge.com>
2016-01-28 17:39:43 +0100

    Go on Linux..

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 //go:generate bin/module-proto
 //go:generate bin/protoc
 //go:generate sh -c "bin/module-mapping > protobuf/access_mapping.go"
-//go:generate cd shared && go-bindata -pkg shared data/...
+//go:generate sh -c "cd shared && go-bindata -pkg shared data/..."
 
 package main
 
