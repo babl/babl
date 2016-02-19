4918126f799dfb9dfc86794ccba342455b97b1d3
Lars Kluge <l@larskluge.com>
2016-02-19 15:09:53 -0600

    Fix babl graph tmp impl

--- babl-graph/main.go
+++ babl-graph/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	m := shared.NewModule("string-append")
+	m := shared.NewModule("string-append", false)
 	m.Env = map[string]string{
 		"APPENDIX": "bar",
 	}
 	stdout, _, _, _ := m.Call(shared.ReadStdin())
 
-	m2 := shared.NewModule("string-upcase")
+	m2 := shared.NewModule("string-upcase", false)
 	stdout2, _, _, _ := m2.Call(stdout)
 
 	fmt.Println(string(stdout2))
