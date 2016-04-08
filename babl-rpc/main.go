0636112c5ee66bac49e0114bd63376ec11cdc557
Lars Kluge <l@larskluge.com>
2016-04-07 22:37:44 -0500

    Less verbose

--- babl-rpc/main.go
+++ babl-rpc/main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 
 	"github.com/DavidHuie/quartz/go/quartz"
-	"github.com/larskluge/babl/log"
 	"github.com/larskluge/babl/shared"
 )
 
@@ -35,7 +34,6 @@ func (_ *Babl) Module(req ModuleRequest, response *ModuleResponse) error {
 		m.Env = req.Env
 
 		stdin, err := base64.StdEncoding.DecodeString(req.Stdin)
-		log.Printf("babl-rpc: Received %d bytes decoded data for Stdin", len(stdin))
 		if err != nil {
 			return err
 		}
