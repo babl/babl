98d01258951f5b8df5d9f881be16c97807eb7c08
Lars Kluge <l@larskluge.com>
2016-04-21 19:08:16 -0500

    Fix rpc test

--- babl-rpc/test.rb
+++ babl-rpc/test.rb
@@ -7,5 +7,5 @@ puts "Structs: #{client.structs}"
 babl = client[:babl]
 puts "Struct methods for #{babl.struct_name}: #{babl.struct_methods}"
 puts "Response:"
-res = babl.call('Module', 'Name' => 'string-upcase', 'Stdin' => Base64.encode64('Send reinforcements'))
+res = babl.call('Module', 'Name' => 'larskluge/string-upcase', 'Stdin' => Base64.encode64('Send reinforcements'))
 puts Base64.decode64(res["Stdout"])
