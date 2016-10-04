9fc8016f23ed92808c97016b6708923d85649007
Lars Kluge <l@larskluge.com>
2016-10-04 18:30:22 +0200

    Add round robin algorithm for rpc server

---
+++ babl-rpc/roundrobin/rr.go
@@ -0,0 +1,91 @@
+package roundrobin
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Endpoint struct {
+	endpoint string
+	weight   int
+}
+
+type RoundRobin struct {
+	mutex *sync.Mutex
+	// Current index (starts from -1)
+	index         int
+	endpoints     []Endpoint
+	currentWeight int
+}
+
+func New(endpoints []Endpoint) (*RoundRobin, error) {
+	if len(endpoints) == 0 {
+		return nil, fmt.Errorf("no endpoints in the pool")
+	}
+	rr := &RoundRobin{
+		index:     -1,
+		mutex:     &sync.Mutex{},
+		endpoints: endpoints,
+	}
+	return rr, nil
+}
+
+func (r *RoundRobin) NextEndpoint() string {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	// The algo below may look messy, but is actually very simple
+	// it calculates the GCD  and subtracts it on every iteration, what interleaves endpoints
+	// and allows us not to build an iterator every time we readjust weights
+
+	// GCD across all enabled endpoints
+	gcd := r.weightGcd()
+	// Maximum weight across all enabled endpoints
+	max := r.maxWeight()
+
+	for {
+		r.index = (r.index + 1) % len(r.endpoints)
+		if r.index == 0 {
+			r.currentWeight = r.currentWeight - gcd
+			if r.currentWeight <= 0 {
+				r.currentWeight = max
+				if r.currentWeight == 0 {
+					panic("all endpoints have 0 weight")
+				}
+			}
+		}
+		ep := r.endpoints[r.index]
+		if ep.weight >= r.currentWeight {
+			return ep.endpoint
+		}
+	}
+}
+
+func (rr *RoundRobin) maxWeight() int {
+	max := -1
+	for _, e := range rr.endpoints {
+		if e.weight > max {
+			max = e.weight
+		}
+	}
+	return max
+}
+
+func (rr *RoundRobin) weightGcd() int {
+	divisor := -1
+	for _, e := range rr.endpoints {
+		if divisor == -1 {
+			divisor = e.weight
+		} else {
+			divisor = gcd(divisor, e.weight)
+		}
+	}
+	return divisor
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
---
+++ babl-rpc/roundrobin/rr_test.go
@@ -0,0 +1,54 @@
+package roundrobin
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+func TestRR(t *testing.T) { TestingT(t) }
+
+type RRSuite struct{}
+
+var _ = Suite(&RRSuite{})
+
+func (s *RRSuite) TestNoServers(c *C) {
+	_, err := New([]Endpoint{})
+	c.Assert(err, NotNil)
+}
+
+func (s *RRSuite) TestOneServer(c *C) {
+	lb, err := New([]Endpoint{Endpoint{endpoint: "a", weight: 1}})
+	c.Assert(err, IsNil)
+
+	c.Assert(seq(c, lb, 3), DeepEquals, []string{"a", "a", "a"})
+}
+
+func (s *RRSuite) TestSimple(c *C) {
+	lb, err := New([]Endpoint{{"a", 1}, {"b", 1}})
+	c.Assert(err, IsNil)
+
+	c.Assert(seq(c, lb, 3), DeepEquals, []string{"a", "b", "a"})
+}
+
+func (s *RRSuite) TestWeighted(c *C) {
+	lb, err := New([]Endpoint{{"a", 3}, {"b", 2}})
+	c.Assert(err, IsNil)
+
+	c.Assert(seq(c, lb, 6), DeepEquals, []string{"a", "a", "b", "a", "b", "a"})
+}
+
+func (s *RRSuite) TestWeighted2(c *C) {
+	lb, err := New([]Endpoint{{"a", 8}, {"b", 2}})
+	c.Assert(err, IsNil)
+
+	c.Assert(seq(c, lb, 11), DeepEquals, []string{"a", "a", "a", "a", "b", "a", "a", "a", "a", "b", "a"})
+}
+
+func seq(c *C, lb *RoundRobin, repeat int) []string {
+	out := []string{}
+	for i := 0; i < repeat; i++ {
+		out = append(out, lb.NextEndpoint())
+	}
+	return out
+}
