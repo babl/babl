b17f045743cc25f0bb13199f6b2543c08654c869
darkua <sergioveiga@gmail.com>
2016-10-12 05:38:10 +0200

    reuse upgrade for update to desired version

--- bablutils/upgrade.go
+++ bablutils/upgrade.go
@@ -10,20 +10,32 @@ import (
 	"os/exec"
 	"regexp"
 	"runtime"
+	"strings"
 	"syscall"
 )
 
 type Upgrade struct {
-	App string
+	App  string
+	Args []string
+	url  string
 }
 
-func NewUpgrade(app string) Upgrade {
-	return Upgrade{App: app}
+func NewUpgrade(app string, args []string) Upgrade {
+	return Upgrade{App: app, Args: args}
 }
 
 func (u *Upgrade) Upgrade(currentVersion string) {
 	if u.LatestVersionRunning(currentVersion) {
 		fmt.Println("Already up-to-date.")
+	} else {
+		u.url = u.LatestBinaryUrl()
+		u.Update(currentVersion, u.LatestVersion())
+	}
+
+}
+
+func (u *Upgrade) Update(currentVersion string, newVersion string) {
+	if newVersion == "" || currentVersion == newVersion {
 		return
 	}
 
@@ -32,8 +44,10 @@ func (u *Upgrade) Upgrade(currentVersion string) {
 
 	tmpfile, err := ioutil.TempFile("", "upgrading-"+u.App)
 	check(err)
-
-	resp, err := http.Get(u.BinaryUrl())
+	if u.url == "" {
+		u.url = u.ReleaseBinaryUrl(newVersion)
+	}
+	resp, err := http.Get(u.url)
 	check(err)
 	defer resp.Body.Close()
 
@@ -48,14 +62,32 @@ func (u *Upgrade) Upgrade(currentVersion string) {
 	check(err)
 	os.Chmod(tmpfile.Name(), info.Mode())
 
-	err = syscall.Exec(mv, []string{"mv", tmpfile.Name(), AppPath()}, os.Environ())
-	check(err)
+	if u.NewBinaryValid(tmpfile.Name(), newVersion) {
+		cmd := exec.Command(mv, []string{tmpfile.Name(), AppPath()}...)
+		err = cmd.Run()
+		check(err)
+
+		if len(u.Args) != 0 {
+			err = syscall.Exec(u.Args[0], u.Args, os.Environ())
+			check(err)
+		}
+		return
+	} else {
+		fmt.Println("Downloaded binary seems to be corrupt or wrong version, please try again later.")
+		os.Exit(-1)
+	}
 }
 
-func (u *Upgrade) BinaryUrl() string {
+func (u *Upgrade) LatestBinaryUrl() string {
 	goos := runtime.GOOS
 	goarch := runtime.GOARCH
 	return fmt.Sprintf("http://s3.amazonaws.com/babl/%s_%s_%s.gz", u.App, goos, goarch)
+}
+
+func (u *Upgrade) ReleaseBinaryUrl(version string) string {
+	goos := runtime.GOOS
+	goarch := runtime.GOARCH
+	return fmt.Sprintf("http://s3.amazonaws.com/babl/releases/%s/%s_%s_%s.gz", version, u.App, goos, goarch)
 }
 
 func (u *Upgrade) latestVersionUrl() string {
@@ -80,4 +112,10 @@ func (u *Upgrade) LatestVersion() string {
 
 func (u *Upgrade) LatestVersionRunning(version string) bool {
 	return u.LatestVersion() == version
+}
+
+func (u *Upgrade) NewBinaryValid(file string, upgradeVersion string) bool {
+	out, err := exec.Command(file, "-plainversion").Output()
+	check(err)
+	return upgradeVersion == strings.TrimSpace(string(out))
 }
